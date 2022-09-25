2de7a726514c7c56d1fc0ba0487eada5458474b5
Stkoelsn <konrad-martin-e@web.de>
2022-09-25 20:36:35 +0200

    Updates

---
+++ precache-manifest.fbdaff0408fd9fd394cef482f6ad730d.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "933f04690e080fe89541",
+    "url": "/CaH/static/css/main.6dcc287e.chunk.css"
+  },
+  {
+    "revision": "933f04690e080fe89541",
+    "url": "/CaH/static/js/main.6cae37c2.chunk.js"
+  },
+  {
+    "revision": "71f56295eb1fd0b9d249",
+    "url": "/CaH/static/js/runtime~main.fdc71bac.js"
+  },
+  {
+    "revision": "9e872c5da5969d819180",
+    "url": "/CaH/static/js/2.0e885417.chunk.js"
+  },
+  {
+    "revision": "ae9de2e797326c8436a7",
+    "url": "/CaH/static/js/3.5d3ae9cd.chunk.js"
+  },
+  {
+    "revision": "bad790e30f259275d05ea025f6e56643",
+    "url": "/CaH/static/media/win.bad790e3.gif"
+  },
+  {
+    "revision": "0db8981a6d48952506b0c574ec204cac",
+    "url": "/CaH/static/media/UnitedKingdom.0db8981a.png"
+  },
+  {
+    "revision": "ce627ccff9a749a11d1b0f8b336d5175",
+    "url": "/CaH/static/media/createGame.ce627ccf.png"
+  },
+  {
+    "revision": "1f4ad68f60628e89c7b87e95724865de",
+    "url": "/CaH/static/media/menu.1f4ad68f.png"
+  },
+  {
+    "revision": "5b14b93d402cd0f79ebbf3601cf7a270",
+    "url": "/CaH/static/media/menu2.5b14b93d.png"
+  },
+  {
+    "revision": "3a4cf24b22b3d4a84ec55aa13402af72",
+    "url": "/CaH/static/media/joinGame.3a4cf24b.png"
+  },
+  {
+    "revision": "c5649144126a6814b702d40f46259b1c",
+    "url": "/CaH/static/media/inGame.c5649144.png"
+  },
+  {
+    "revision": "491573ab0b9a9937b36775bc52a69e7f",
+    "url": "/CaH/index.html"
+  }
+];
